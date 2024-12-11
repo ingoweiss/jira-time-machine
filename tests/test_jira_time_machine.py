22b239d3a958634ae59eba043eace33282176568
Ingo Weiss <ingo@ingoweiss.com>
2024-12-11 08:42:30 -0500

    add failing tests for problems with fetch_backlog_history

--- tests/test_jira_time_machine.py
+++ tests/test_jira_time_machine.py
@@ -23,3 +23,15 @@ def test_fetch_backlog_history(jira_time_machine):
     assert len(records[records["Record", "type"] == "created"]) == 1, "Should have 1 'created' record for PROJ-0001"
     assert len(records[records["Record", "type"] == "change"]) == 2, "Should have 2 'change' records for PROJ-0001"
     assert len(records[records["Record", "type"] == "current"]) == 1, "Should have 1 'current' record for PROJ-0001"
+
+    proj_0001_current = history_df[(history_df[('Record', 'issue_id')] == 'PROJ-0001') & (history_df[('Record', 'type')] == 'current')].iloc[0]
+    assert proj_0001_current[('Tracked Fields', 'status')] == "Submitted", "PROJ-0001 current status should be 'Submitted'"
+    assert proj_0001_current[('Tracked Fields', 'priority')] == "Major", "PROJ-0001 current priority should be 'Major'"
+
+    proj_0001_last_change = history_df[(history_df[('Record', 'issue_id')] == 'PROJ-0001') & (history_df[('Record', 'type')] == 'change')].iloc[-1]
+    assert proj_0001_last_change[('Tracked Fields', 'status')] == "Submitted", "PROJ-0001 current status should be 'Submitted'"
+    assert proj_0001_last_change[('Tracked Fields', 'priority')] == "Major", "PROJ-0001 current priority should be 'Major'"
+
+    proj_0001_created = history_df[(history_df[('Record', 'issue_id')] == 'PROJ-0001') & (history_df[('Record', 'type')] == 'change')].iloc[-1]
+    assert proj_0001_created[('Tracked Fields', 'status')] == "New", "PROJ-0001 current status should be 'New'"
+    assert proj_0001_created[('Tracked Fields', 'priority')] == "Minor", "PROJ-0001 current priority should be 'Minor'"
